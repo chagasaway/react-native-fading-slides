9beb434b33e1835883200b65b1d4777799cc386b
Fellipe Chagas <chagas.0@gmail.com>
2015-10-18 01:11:08 -0200

    Update Android example

--- example/index.android.js
+++ example/index.android.js
@@ -5,26 +5,48 @@
 'use strict';
 
 var React = require('react-native');
+var Dimensions = require('Dimensions');
+var {width, height} = Dimensions.get('window');
+
+var FadingSlides = require('react-native-fading-slides');
+
 var {
   AppRegistry,
   StyleSheet,
-  Text,
   View,
 } = React;
+
+var slides = [
+  {
+    image: require('image!javascript'),
+    imageWidth: width - (width * 0.3),
+    imageHeight: width - (width * 0.3),
+    title: 'JavaScript',
+    subtitle: 'The definitive language',
+    titleColor: '#fff',
+    subtitleColor: 'yellow'
+  },
+  {
+    image: require('image!react'),
+    imageWidth: width - (width * 0.3),
+    imageHeight: width - (width * 0.3),
+    title: 'ReactJS',
+    subtitle: 'JavaScript coolest library',
+    titleColor: '#fff',
+    subtitleColor: 'cyan'
+  }
+];
 
 var example = React.createClass({
   render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <FadingSlides
+          slides={slides}
+          fadeDuration={1200}
+          stillDuration={2000}
+          height={height}
+        />
       </View>
     );
   }
@@ -35,18 +57,8 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#000',
+  }
 });
 
 AppRegistry.registerComponent('example', () => example);
