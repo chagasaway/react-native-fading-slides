808dbafff2e048d4d5e25eff728355e774bb36ad
Fellipe Chagas <chagas.0@gmail.com>
2017-12-11 00:50:55 -0200

    Removes unused dependencie from example

--- example/App.js
+++ example/App.js
@@ -1,41 +1,36 @@
-import React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
 
-import FadingSlides from "react-native-fading-slides";
+import FadingSlides from 'react-native-fading-slides';
 
-const { width, height } = Dimensions.get("window");
+const { width, height } = Dimensions.get('window');
 
 const slides = [
   {
-    image: require("./img/javascript.png"),
+    image: require('./img/javascript.png'),
     imageWidth: width - width * 0.3,
     imageHeight: width - width * 0.3,
-    title: "JavaScript",
-    subtitle: "The definitive language",
-    titleColor: "#fff",
-    subtitleColor: "yellow"
+    title: 'JavaScript',
+    subtitle: 'The definitive language',
+    titleColor: '#fff',
+    subtitleColor: 'yellow',
   },
   {
-    image: require("./img/react.png"),
+    image: require('./img/react.png'),
     imageWidth: width - width * 0.3,
     imageHeight: width - width * 0.3,
-    title: "ReactJS",
-    subtitle: "JavaScript coolest library",
-    titleColor: "#fff",
-    subtitleColor: "cyan"
-  }
+    title: 'ReactJS',
+    subtitle: 'JavaScript coolest library',
+    titleColor: '#fff',
+    subtitleColor: 'cyan',
+  },
 ];
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <FadingSlides
-          slides={slides}
-          fadeDuration={1200}
-          stillDuration={2000}
-          height={height}
-        />
+        <FadingSlides slides={slides} fadeDuration={1200} stillDuration={2000} height={height} />
       </View>
     );
   }
@@ -44,8 +39,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#000"
-  }
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000',
+  },
 });
